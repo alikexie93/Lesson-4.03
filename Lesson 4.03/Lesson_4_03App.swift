2a5cac635f499c9214bd4824f2a74d8c118b3a50
aleksei <alikexie93@yandex.ru>
2023-06-14 23:40:44 +0600

    Initial Commit

---
+++ Lesson 4.03/Lesson_4_03App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_03App.swift
+//  Lesson 4.03
+//
+//  Created by Алексей Филиппов on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
